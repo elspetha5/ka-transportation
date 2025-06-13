333e97d83d86d3162512524ec14a42c05931f559
Elspeth Anderson <elspeth.anderson@discounttire.com>
2025-06-13 16:32:20 -0700

    took out base route config

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   const config = {
     plugins: [react()],
   };
-
-  if (command === "build") {
-    config.base = "/ka-transportation/";
-  }
 
   return config;
 });
